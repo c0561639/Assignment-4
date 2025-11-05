b7f653c8f18114999bb650b772d05f3a96f17a74
Sean Clarke <c0561639@mylambton.ca>
2025-11-04 20:37:29 -0500

    Final version commit
    Updated index.html, app.py to reflect changes, added readme

---
+++ README.md
@@ -0,0 +1,3 @@
+App is super basic. Shows APP_MESSAGE & APP_HEALTH of which is pulled from .env file. 
+You are able to click either APP_MESSAGE or APP_HEALTH to go to their respective
+plain pages.
--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from dotenv import load_dotenv; load_dotenv()
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 import os
 
 app = Flask(__name__)
 
+def get_message():
+    return os.getenv("APP_MESSAGE", "Howdy!")
+
+def get_health():
+    return os.getenv("APP_HEALTH", "OK")
+
 @app.get("/")
 def home():
-    message = os.getenv("APP_MESSAGE", "Hello from Flask (set APP_MESSAGE)!")
-    return Response(message, mimetype="text/plain")
+    return render_template("index.html", message=get_message(), health=get_health())
+
+@app.get("/message")
+def message():
+    return get_message()
 
 @app.get("/health")
 def health():
-    status = os.getenv("APP_HEALTH", "OK")
-    return Response(status, mimetype="text/plain")
+    return get_health()
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "5000"))
